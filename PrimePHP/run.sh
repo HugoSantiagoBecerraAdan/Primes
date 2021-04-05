52ef917e1a90496d229e5a84814f002ccdb4a30c
Hugo Santiago Becerra Adán <hsbecerra@vocento.com>
2021-04-05 16:13:19 +0200

    Update PHP implementation with CPP changes

--- PrimePHP/run.sh
+++ PrimePHP/run.sh
@@ -1,1 +1,1 @@
-php8.0 -dopcache.enable_cli=1 -dopcache.enable=1 -dopcache.jit_buffer_size=100M -dopcache.jit=1255 -dxdebug.mode=off -derror_reporting=1 PrimePHP.php
+php -dopcache.enable_cli=1 -dopcache.enable=1 -dopcache.jit_buffer_size=100M -dopcache.jit=1255 -dxdebug.mode=off PrimePHP.php
